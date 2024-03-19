f5b747ebbbdcc39dacd8eda98c6646ba7dbd36f8
zero <39059459+WilSegale@users.noreply.github.com>
2024-03-19 17:01:42 -0400

    Update services Status.py

--- services Status.py
+++ services Status.py
@@ -2,11 +2,11 @@ from DontEdit import *
 import sys
 import time
 def service():
-    x = subprocess.run("service", "--status-all")
+    x = subprocess.run(["service", "--status-all"])
 
     for i in x:
         print(i)
-        
+
 if platform.system() == linux:
     service()
 else:
