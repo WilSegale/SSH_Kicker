09fd7aa6fdd5a63a71216f8b1ed7a01b04561cc3
zero <39059459+WilSegale@users.noreply.github.com>
2024-03-19 17:00:01 -0400

    Update services Status.py

--- services Status.py
+++ services Status.py
@@ -2,10 +2,17 @@ from DontEdit import *
 import sys
 import time
 def service():
-    subprocess.run("sudo","service","--list")
+    subprocess.run("service", "--status-all")
+
+    for i in range(subprocess.run("service","--status-all")):
+        print(i)
+        time.sleep(1)
+        sys.stdout.write("\r")
+        sys.stdout.write(str(i))
+        sys.stdout.flush()
+
 
 if platform.system() == linux:
     service()
 else:
     print("This program only works on Linux computers")
-
