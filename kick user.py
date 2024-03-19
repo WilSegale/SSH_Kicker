db41a6bcd7104a18bbe6273f2152066d9d661ecc
zero <39059459+WilSegale@users.noreply.github.com>
2024-03-19 15:08:43 -0400

    kick user update

--- kick user.py
+++ kick user.py
@@ -2,59 +2,62 @@ import subprocess
 import os
 import sys
 
-#get the info from the "accie art" file
-WarningLogo = open("accie art.txt")
-
-# color vars
+# Color variables
 BRIGHT = '\033[1m'
 GREEN = "\033[92m"
 RED = "\033[91m"
 ORANGE = "\033[38;2;255;165;0m"
 RESET = "\033[0m"
 
-# Esay way to check if the script is run with sudo privileges
 ROOT = 0
 
-KICKED = open("KICKED_USERS.txt", "w")
+def get_ssh_pid(ip_address):
+    try:
 
-# Check if the script is run with sudo privileges
-if os.geteuid() != ROOT:
-    print(f"{ORANGE}{WarningLogo.read()}{RESET}")
-    print()
-    print("This script requires sudo privileges to run.")
-    print("Please run the script again with sudo.")
-    sys.exit(1)
-else:
-    who = subprocess.check_output(['who']).decode('utf-8')
-    print(who)
-    def get_ssh_pid(ip_address):
-        try:
-            # Get the PID of the SSH session associated with the given IP address
-            pid_info = subprocess.check_output(['netstat', '-tnpa']).decode('utf-8')
-            for line in pid_info.split('\n'):
-                if 'ESTABLISHED' in line and ip_address in line:
-                    pid = line.split()[6].split('/')[0]
-                    return pid
-        except subprocess.CalledProcessError:
-            print("Error: Unable to retrieve PID for IP address", ip_address)
-        return None
-
-    def kick_user(ip_address):
         # Get the PID of the SSH session associated with the given IP address
-        pid = get_ssh_pid(ip_address)
-        if pid:
-            try:
-                # Terminate the SSH session
-                subprocess.run(['sudo', 'kill', pid])
-                print(f"SSH session associated with {BRIGHT}{GREEN}{ip_address}{RESET} has been terminated.", file=KICKED)
-                print(f"We have kicked {BRIGHT}{GREEN}{UserToKick}{RESET} off your computer", file=KICKED)
-            except subprocess.CalledProcessError:
-                print(f"[ {RED}FAIL{RESET} ]: Unable to terminate SSH session for IP address {ip_address}", file=KICKED)
-        else:
-            print(f"[ {RED}FAIL{RESET} ] No active SSH session found for IP address {ip_address}", file=KICKED)
+        pid_info = subprocess.check_output(['netstat', '-tnpa']).decode('utf-8')
+        for line in pid_info.split('\n'):
+            if 'ESTABLISHED' in line and ip_address in line:
+                pid = line.split()[6].split('/')[0]
+                return pid
+    except subprocess.CalledProcessError:
+        print("Error: Unable to retrieve PID for IP address", ip_address)
+    return None
 
-    if __name__ == "__main__":
-        # what the user inputs in the "UserToKick" it kick the user off the computer
-        UserToKick = input("Input the IP: ")
-        # calls to the kick user funciton
-        kick_user(f"{UserToKick}")
+def kick_user(ip_address):
+    pid = get_ssh_pid(ip_address)
+    if pid:
+        try:
+            # Terminate the SSH session
+            subprocess.run(['sudo', 'kill', pid])
+            print(f"SSH session associated with {BRIGHT}{GREEN}{ip_address}{RESET} has been terminated.")
+            with open("KICKED_USERS.txt", "a") as kicked_users_file:
+                print(f"We have kicked {BRIGHT}{GREEN}{ip_address}{RESET} off your computer")
+                print(f"We have kicked {BRIGHT}{GREEN}{ip_address}{RESET} off your computer", file=kicked_users_file)
+        except subprocess.CalledProcessError:
+            print(f"[ {RED}FAIL{RESET} ]: Unable to terminate SSH session for IP address {ip_address}")
+    else:
+        print(f"[ {RED}FAIL{RESET} ] No active SSH session found for IP address {ip_address}")
+
+if __name__ == "__main__":
+    # Check if the script is run with sudo privileges
+    if os.geteuid() != ROOT:
+        print(f"{ORANGE}This script requires sudo privileges to run.{RESET}")
+        sys.exit(1)
+    else:
+        who = subprocess.check_output(['who']).decode('utf-8')
+        print(who)
+
+        nslookupOrKick = input("Do you want to nslookup or kick a user? (nslookup/kick): ")
+        
+        if nslookupOrKick == "nslookup":
+            nslookup = input("Enter the domain to lookup: ")
+            subprocess.run(['nslookup', nslookup])
+            sys.exit(0)  # Exit after nslookup
+
+        print("Enter the IP address of the user you want to kick off:")
+        ip_address = input(">>> ")
+
+        # Add input validation if needed
+
+        kick_user(ip_address)
