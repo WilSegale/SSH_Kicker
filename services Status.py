af75ffbb3a5676d05e2c97f8ed7b2b0862bde076
zero <39059459+WilSegale@users.noreply.github.com>
2024-03-19 17:25:07 -0400

    Update services Status.py

--- services Status.py
+++ services Status.py
@@ -4,8 +4,19 @@ from DontEdit import *
 def service():
     subprocess.run(["service", "--status-all"])
 
-    print("What service would you like to use etc(ssh, apache2)")
+    print("What service would you like to use (ssh, apache2, bluetooth, etc...)")
 
+    services = input(">>> ")
+    print("Would you like to (start, stop, restart, status, --full-restart) the service")
+    settings = input(">>> ")
+    subprocess.run(["service", services, settings])
+    print("Would you like to check the status of the service again?")
+    check = input(">>> ")
+
+    if check == "yes":
+        subprocess.run(["service", services, "status"])
+    else:
+        print("Thank you for using this program")
 #checks if the user is on linux or not
 if platform.system() == linux:
     service()
